609293b957a57d8d669464d53da7019c5d177ea5
Hollylord <inc@appletekiMacBook-Pro.local>
2015-08-13 14:47:16 +0800

    首页新增搜索框

--- touwho_iPad/touwho_iPad/programViewController.m
+++ touwho_iPad/touwho_iPad/programViewController.m
@@ -7,8 +7,20 @@
 //
 
 #import "programViewController.h"
+#import "INSSearchBar.h"
 
-@interface programViewController ()
+@interface programViewController () <INSSearchBarDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *headIcon;
+@property (weak, nonatomic) IBOutlet UIButton *programBtn;
+@property (weak, nonatomic) IBOutlet UIButton *newsBtn;
+@property (weak, nonatomic) IBOutlet UIButton *findBtn;
+@property (weak, nonatomic) IBOutlet UIButton *meBtn;
+@property (weak,nonatomic) INSSearchBar *searchBar;
+
+- (IBAction)program:(UIButton *)sender;
+- (IBAction)news:(UIButton *)sender;
+- (IBAction)find:(UIButton *)sender;
+- (IBAction)me:(UIButton *)sender;
 
 @end
 
@@ -17,6 +29,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    INSSearchBar *search = [[INSSearchBar alloc] initWithFrame:CGRectMake(744, 42, 44, 30)];
+    [self.view addSubview:search];
+    self.searchBar = search;
+    self.searchBar.delegate = self;
+    self.searchBar.backgroundColor = [UIColor redColor];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,6 +42,47 @@
     
 }
 
+
+#pragma mark - 按钮点击
+- (IBAction)program:(UIButton *)sender {
+}
+
+- (IBAction)news:(UIButton *)sender {
+}
+
+- (IBAction)find:(UIButton *)sender {
+}
+
+- (IBAction)me:(UIButton *)sender {
+}
+
+#pragma mark - 搜索框的代理
+- (CGRect)destinationFrameForSearchBar:(INSSearchBar *)searchBar
+{
+    return CGRectMake(744, 42, 250, 30);
+}
+
+- (void)searchBar:(INSSearchBar *)searchBar willStartTransitioningToState:(INSSearchBarState)destinationState
+{
+    // Do whatever you deem necessary.
+}
+
+- (void)searchBar:(INSSearchBar *)searchBar didEndTransitioningFromState:(INSSearchBarState)previousState
+{
+    // Do whatever you deem necessary.
+}
+
+- (void)searchBarDidTapReturn:(INSSearchBar *)searchBar
+{
+    // Do whatever you deem necessary.
+    // Access the text from the search bar like searchBar.searchField.text
+}
+
+- (void)searchBarTextDidChange:(INSSearchBar *)searchBar
+{
+    // Do whatever you deem necessary.
+    // Access the text from the search bar like searchBar.searchField.text
+}
 
 
 @end
