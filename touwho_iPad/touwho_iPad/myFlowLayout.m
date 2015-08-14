5979e684c96aa00ab0963c347ab160cd5496a2f3
Hollylord <inc@appletekiMacBook-Pro.local>
2015-08-14 13:39:21 +0800

    图片collection布局完成

--- touwho_iPad/touwho_iPad/myFlowLayout.m
+++ touwho_iPad/touwho_iPad/myFlowLayout.m
@@ -7,49 +7,100 @@
 //
 
 #import "myFlowLayout.h"
+#import "CATransform3DPerspect.h"
+
+
+#define ACTIVE_DISTANCE 550
+#define CELL_DISTANCE 80
 
 @implementation myFlowLayout
 
--(id)init
-{
-    self = [super init];
+- (instancetype)initWithCoder:(NSCoder *)aDecoder{
+    self = [super initWithCoder:aDecoder];
     if (self) {
-//        self.itemSize = CGSizeMake(200,200);
-        self.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+//        self.scrollDirection = UICollectionViewScrollDirectionHorizontal;
 //        self.sectionInset = UIEdgeInsetsMake(200, 0.0, 200, 0.0);
-        //左右间距
-        self.minimumLineSpacing = 50.0;
+        self.minimumLineSpacing = -40.0;
     }
     return self;
 }
+
+//-(id)init
+//{
+//    self = [super init];
+//    if (self) {
+//        self.itemSize = CGSizeMake(ITEM_SIZE, 200);
+//        self.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+//        self.sectionInset = UIEdgeInsetsMake(200, 0.0, 200, 0.0);
+//        self.minimumLineSpacing = -400.0;
+//    }
+//    return self;
+//}
+
 - (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)oldBounds
 {
     return YES;
 }
-//可视范围的布局属性，rect是它的bounds（以父view作为坐标系）
+
 -(NSArray*)layoutAttributesForElementsInRect:(CGRect)rect
 {
     NSArray* array = [super layoutAttributesForElementsInRect:rect];
     CGRect visibleRect;
-    //visibleRect以自己作为坐标系
     visibleRect.origin = self.collectionView.contentOffset;
     visibleRect.size = self.collectionView.bounds.size;
     
     for (UICollectionViewLayoutAttributes* attributes in array) {
         if (CGRectIntersectsRect(attributes.frame, rect)) {
             CGFloat distance = CGRectGetMidX(visibleRect) - attributes.center.x;
-            CGFloat normalizedDistance = distance / 200;
-            if (ABS(distance) < 200) {
-                CGFloat zoom = 1 + 0.3*(1 - ABS(normalizedDistance));
-                attributes.transform3D = CATransform3DMakeScale(zoom, zoom, 0.5);
-                attributes.zIndex = 0;
+            CGFloat normalizedDistance = distance / ACTIVE_DISTANCE;
+            if (ABS(distance) < ACTIVE_DISTANCE) {
+                
+                CGFloat zoom = 1 - ABS(normalizedDistance);
+                NSLog(@"%f",zoom);
+                if (distance > 0) {
+                    CATransform3D rotate = CATransform3DMakeRotation((1-zoom)*M_PI/7, 0, 1, 0);
+                    
+                    attributes.transform3D = rotate;
+                    CGFloat zoom2 = zoom +0.8;
+                    if (zoom2 < 1.4) {
+                        attributes.transform3D = CATransform3DPerspect(rotate, CGPointMake(0,0.5), CELL_DISTANCE,zoom2);
+                    }
+                    else {
+                        attributes.transform3D = CATransform3DPerspect(rotate, CGPointMake(0,0.5), CELL_DISTANCE,1.4);
+                    }
+                    
+                    
+                    
+                    attributes.zIndex = 1;
+                }
+                else {
+                    CATransform3D rotate = CATransform3DMakeRotation((zoom-1)*M_PI/7, 0, 1, 0);
+                    
+                    attributes.transform3D = rotate;
+                    
+                    CGFloat zoom2 = zoom +0.8;
+                    if (zoom2 < 1.4) {
+                        attributes.transform3D = CATransform3DPerspect(rotate, CGPointMake(0,0.5), CELL_DISTANCE,zoom2);
+                    }
+                    else {
+                        attributes.transform3D = CATransform3DPerspect(rotate, CGPointMake(0,0.5), CELL_DISTANCE,1.4);
+                    }
+                    
+                    attributes.zIndex = 1;
+                }
+                
+            }
+            else{
+                [UIView animateWithDuration:1 animations:^{
+                    attributes.transform3D = CATransform3DIdentity;
+                }];
             }
         }
     }
     return array;
 }
 
-//自动对齐到网格
+
 - (CGPoint)targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset withScrollingVelocity:(CGPoint)velocity
 {
     CGFloat offsetAdjustment = MAXFLOAT;
