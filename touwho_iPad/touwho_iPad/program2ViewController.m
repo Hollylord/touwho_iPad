9adba3fc7ffc4988b13d11bb15191466c577a13c
Hollylord <inc@appletekiMacBook-Pro.local>
2015-09-11 13:42:47 +0800

    项目详情页面添加关注按钮
    原来不能在viewDidLoad方法中重复加载自己的控制器

--- touwho_iPad/touwho_iPad/program2ViewController.m
+++ touwho_iPad/touwho_iPad/program2ViewController.m
@@ -18,13 +18,27 @@
 @end
 
 @implementation program2ViewController
+{
+    programView *viewForprogram;//左上角的项目视图
+    UIButton *btn;//关注按钮
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+
+    //添加programView视图
     programView *view = [[[NSBundle mainBundle] loadNibNamed:@"programView" owner:nil options:nil]firstObject];
     view.backgroundColor = [UIColor redColor];
     [self.program addSubview:view];
+    viewForprogram = view;
+    
+    //添加关注按钮
+    UIButton *followBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [view addSubview:followBtn];
+    followBtn.backgroundColor = [UIColor greenColor];
+    [followBtn setTitle:@"关注" forState:UIControlStateNormal];
+    btn = followBtn;
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,24 +48,36 @@
 
 - (void)viewWillAppear:(BOOL)animated{
     [self.navigationController setNavigationBarHidden:NO animated:YES];
+    
 }
 
 /**
- *  控制器的子视图更新约束
+ *  控制器的子视图更新约束(添加一些新视图的约束)
  */
 - (void)updateViewConstraints{
+    
     [super updateViewConstraints];
     
-    /** 给programview添加约束**/
-    programView *view = self.program.subviews[0];
-    [self layoutForProgram:view];
+    [self layoutForFollowBtn:btn];
+    
+    [self layoutForProgram:viewForprogram];
+    
 }
 - (void)layoutForProgram:(programView *)view{
-    NSLayoutConstraint *leading = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:view.superview attribute:NSLayoutAttributeLeading multiplier:1 constant:0];
-    NSLayoutConstraint *trailing = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:view.superview attribute:NSLayoutAttributeTrailing multiplier:1 constant:0];
-    NSLayoutConstraint *top = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:view.superview attribute:NSLayoutAttributeTop multiplier:1 constant:0];
-    NSLayoutConstraint *bottom = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:view.superview attribute:NSLayoutAttributeBottom multiplier:1 constant:0];
-    [view.superview addConstraints:@[leading,trailing,top,bottom]];
+    NSLayoutConstraint *leading = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.program attribute:NSLayoutAttributeLeading multiplier:1 constant:0];
+    NSLayoutConstraint *trailing = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.program attribute:NSLayoutAttributeTrailing multiplier:1 constant:0];
+    NSLayoutConstraint *top = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.program attribute:NSLayoutAttributeTop multiplier:1 constant:0];
+    NSLayoutConstraint *bottom = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.program attribute:NSLayoutAttributeBottom multiplier:1 constant:0];
+    [self.program addConstraints:@[leading,trailing,top,bottom]];
     view.translatesAutoresizingMaskIntoConstraints = NO;
+}
+- (void)layoutForFollowBtn:(UIView *)view {
+    NSLayoutConstraint *trailing = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:view.superview attribute:NSLayoutAttributeTrailing multiplier:1 constant:-20];
+    NSLayoutConstraint *top = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:view.superview attribute:NSLayoutAttributeTop multiplier:1 constant:20];
+    
+    [view.superview addConstraints:@[trailing,top]];
+    view.translatesAutoresizingMaskIntoConstraints = NO;
+
+
 }
 @end
