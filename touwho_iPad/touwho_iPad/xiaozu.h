662c2f11c05017b6f8cdc483a0d1a8ff0b36c170
Hollylord <inc@appletekiMacBook-Pro.local>
2015-09-14 10:28:26 +0800

    小组新增 我加入的小组列表

--- touwho_iPad/touwho_iPad/xiaozu.h
+++ touwho_iPad/touwho_iPad/xiaozu.h
@@ -13,5 +13,6 @@
 
 @property (weak, nonatomic) IBOutlet UILabel *label2;
 @property (weak, nonatomic) IBOutlet UIImageView *splitLine1;
+@property (weak, nonatomic) IBOutlet UIImageView *splitLine2;
 
 @end
--- touwho_iPad/touwho_iPad/xiaozu.m
+++ touwho_iPad/touwho_iPad/xiaozu.m
@@ -16,14 +16,16 @@
 
 @implementation xiaozu
 {
-    NSMutableArray *hotGroup;
+    NSMutableArray *hotGroup;//热门推荐小组
+    NSMutableArray *iInvolved;//我加入的小组
 }
 
-//只会初始化自己 不能在这里面添加子视图，设置层级关系无效
+//只会初始化自己 不能在这里面添加子视图的子视图，设置层级关系无效。只能设置self 与子视图的层级关系
 - (instancetype)initWithCoder:(NSCoder *)aDecoder{
     self = [super initWithCoder:aDecoder];
     if (self) {
         hotGroup = [NSMutableArray array];
+        iInvolved = [NSMutableArray array];
 
     }
     return self;
@@ -31,18 +33,25 @@
 
 //相当于viewDidLoad
 - (void)awakeFromNib{
+    //添加热门小组
     for (int i = 0; i < 5; i ++) {
         UIView *unit = [[[NSBundle mainBundle] loadNibNamed:@"xiaozuUnit" owner:nil options:nil]firstObject];
         [self.scrollView addSubview:unit];
         [hotGroup addObject:unit];
+    }
+    //添加我加入的小组
+    for (int i = 0; i < 6; i ++) {
+        UIView *unit = [[[NSBundle mainBundle] loadNibNamed:@"xiaozuUnit" owner:nil options:nil]firstObject];
+        [self.scrollView addSubview:unit];
+        [iInvolved addObject:unit];
     }
 }
 
 - (void)updateConstraints{
     [super updateConstraints];
     
+    //布局热门推荐
     NSUInteger totalNumber = hotGroup.count;
-    
     for (int i = 0; i < totalNumber; i ++) {
         //列数
         int column = i%3;
@@ -59,7 +68,24 @@
         view.translatesAutoresizingMaskIntoConstraints = NO;
     }
     
-    
+    //布局我加入的小组
+    NSUInteger totalNumber2 = iInvolved.count;
+    for (int i = 0; i < totalNumber2; i ++) {
+        //列数
+        int column = i%3;
+        //行数
+        int lines = (int) i/3;
+        
+        UIView *view = iInvolved[i];
+        NSLayoutConstraint *leading = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.splitLine2 attribute:NSLayoutAttributeLeading multiplier:1 constant:MarginSide + column * (Width+MarginSide)];
+        NSLayoutConstraint *top = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.splitLine2 attribute:NSLayoutAttributeBottom multiplier:1 constant:MarginTop + lines * (Height + MarginTop)];
+        NSLayoutConstraint *width = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:Width];
+        NSLayoutConstraint *height = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:Height];
+        [view.superview addConstraints:@[leading,top]];
+        [view addConstraints:@[width,height]];
+        view.translatesAutoresizingMaskIntoConstraints = NO;
+    }
+    //布局label2
     for (NSLayoutConstraint *constraint in self.scrollView.constraints) {
         if ([constraint.identifier isEqualToString:@"label2TopConstraint"]) {
             [self.scrollView removeConstraint:constraint];
@@ -67,6 +93,9 @@
             [self.scrollView addConstraint:top];
         }
     }
+    //布局scrollview的竖直滚动范围
+    NSLayoutConstraint *verticalScale = [NSLayoutConstraint constraintWithItem:[iInvolved lastObject] attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.scrollView attribute:NSLayoutAttributeBottom multiplier:1 constant:-20];
+    [self.scrollView addConstraint:verticalScale];
 
 }
 
