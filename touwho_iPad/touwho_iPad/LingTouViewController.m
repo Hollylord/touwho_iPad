0989887c27e30d7ddeb39b295d62729883b8f68a
Hollylord <inc@appletekiMacBook-Pro.local>
2015-09-24 16:28:26 +0800

    添加跟投金额 页面

--- touwho_iPad/touwho_iPad/LingTouViewController.m
+++ touwho_iPad/touwho_iPad/LingTouViewController.m
@@ -10,6 +10,9 @@
 
 @interface LingTouViewController ()
 
+- (IBAction)quit:(UIBarButtonItem *)sender;
+- (IBAction)OK:(UIBarButtonItem *)sender;
+
 @end
 
 @implementation LingTouViewController
@@ -24,14 +27,11 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)quit:(UIBarButtonItem *)sender {
+    [self dismissViewControllerAnimated:YES completion:NULL];
 }
-*/
 
+- (IBAction)OK:(UIBarButtonItem *)sender {
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
 @end
