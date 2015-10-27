8b41208616c59779cea918f21ab1a31ee50d34b9
Hollylord <283228138@qq.com>
2015-10-27 17:48:44 +0800

    新闻列表页面新增下拉刷新功能

--- touwho_iPad/touwho_iPad/news1ViewController.m
+++ touwho_iPad/touwho_iPad/news1ViewController.m
@@ -40,7 +40,12 @@
     [super viewDidLoad];
     //创建加载数据的AFN
     mgr = [AFHTTPRequestOperationManager manager];
-    mgr.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"text/html", nil];  
+    mgr.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"text/html", nil];
+    
+    //添加下拉刷新
+    UIRefreshControl *refresh = [[UIRefreshControl alloc] init];
+    [refresh addTarget:self action:@selector(pullRefresh:) forControlEvents:UIControlEventValueChanged];
+    [self.scrollView addSubview:refresh];
 }
 
 
@@ -53,6 +58,46 @@
     [self.navigationController setNavigationBarHidden:NO animated:YES];
     //小菊花
     [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    [self getNewDataWithFresh:nil];
+    
+}
+
+
+
+- (void)layoutForNewsMenu:(newsMenu *)view index:(int)i{
+    //行数
+    int line = (int)i/2;
+    //列数
+    int column = i%2;
+    
+    NSLayoutConstraint *leading = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.scrollView attribute:NSLayoutAttributeLeading multiplier:1 constant:MarginSide + column*(MarginSide + Width)];
+    NSLayoutConstraint *top = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.scrollView attribute:NSLayoutAttributeTop multiplier:1 constant:0 + line*(MarginTop + Height)];
+    NSLayoutConstraint *width = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:Width];
+    NSLayoutConstraint *height = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:Height];
+    [self.scrollView addConstraints:@[leading,top]];
+    [view addConstraints:@[width,height]];
+    
+    view.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.scrollView layoutIfNeeded];
+    //设置滚动范围
+    self.scrollView.contentSize = CGSizeMake(0, CGRectGetMaxY(view.frame) + 2000);
+}
+
+#pragma mark - newsMenu代理
+//跳转新闻详细页面
+- (void)turn2newsDetail{
+    
+    news2ViewController *viewcontroller = [[news2ViewController alloc]initWithNibName:@"news2ViewController" bundle:nil];
+    [self.navigationController pushViewController:viewcontroller animated:YES];
+
+}
+
+#pragma mark -  下拉刷新
+- (void)pullRefresh:(UIRefreshControl *)refresh{
+    [self getNewDataWithFresh:refresh];
+}
+//获取新数据
+- (void)getNewDataWithFresh:(UIRefreshControl *)refresh{
     //设置参数
     NSDictionary *para = @{@"method":@"getNewsTitle_Pre",@"news_id":@"0"};
     //网络请求
@@ -60,6 +105,10 @@
         
         //关闭小菊花
         [MBProgressHUD hideHUDForView:self.view animated:YES];
+        
+        //情况模型数组
+        self.allNewsArr = nil;
+        
         //获得新闻数组
         NSArray *newsList = [responseObject objectForKey:@"value"];
         for (NSDictionary *dic in newsList) {
@@ -85,39 +134,13 @@
             [self.scrollView addSubview:view];
             [self layoutForNewsMenu:view index:i];
         }
-       
+        
+        //获取数据成功后停止刷新
+        [refresh endRefreshing];
         
         
     } failure:^(AFHTTPRequestOperation * _Nonnull operation, NSError * _Nonnull error) {
         NSLog(@"%@",error);
     }];
-}
-
-- (void)layoutForNewsMenu:(newsMenu *)view index:(int)i{
-    //行数
-    int line = (int)i/2;
-    //列数
-    int column = i%2;
-    
-    NSLayoutConstraint *leading = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.scrollView attribute:NSLayoutAttributeLeading multiplier:1 constant:MarginSide + column*(MarginSide + Width)];
-    NSLayoutConstraint *top = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.scrollView attribute:NSLayoutAttributeTop multiplier:1 constant:0 + line*(MarginTop + Height)];
-    NSLayoutConstraint *width = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:Width];
-    NSLayoutConstraint *height = [NSLayoutConstraint constraintWithItem:view attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:Height];
-    [self.scrollView addConstraints:@[leading,top]];
-    [view addConstraints:@[width,height]];
-    
-    view.translatesAutoresizingMaskIntoConstraints = NO;
-    [self.scrollView layoutIfNeeded];
-    //设置滚动范围
-    self.scrollView.contentSize = CGSizeMake(0, CGRectGetMaxY(view.frame) + 20);
-}
-
-#pragma mark - newsMenu代理
-//跳转新闻详细页面
-- (void)turn2newsDetail{
-    
-    news2ViewController *viewcontroller = [[news2ViewController alloc]initWithNibName:@"news2ViewController" bundle:nil];
-    [self.navigationController pushViewController:viewcontroller animated:YES];
-
 }
 @end
