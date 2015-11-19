d89ec5f8654bc2a89b51b990bb52e298975fc1f2
Hollylord <283228138@qq.com>
2015-11-19 15:49:54 +0800

    测试一下合并

---
+++ touwho_iPad/touwho_iPad/test.m
@@ -0,0 +1,21 @@
+//
+//  test.m
+//  touwho_iPad
+//
+//  Created by apple on 15/11/19.
+//  Copyright © 2015年 touhu.com. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
