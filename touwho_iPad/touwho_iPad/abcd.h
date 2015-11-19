df6c92d31cc8b22cddf5295b288b220e3b4909c7
Hollylord <283228138@qq.com>
2015-11-19 15:23:55 +0800

    测试以下分支

---
+++ touwho_iPad/touwho_iPad/abcd.h
@@ -0,0 +1,13 @@
+//
+//  abcd.h
+//  touwho_iPad
+//
+//  Created by apple on 15/11/19.
+//  Copyright © 2015年 touhu.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface abcd : UITableView
+
+@end
---
+++ touwho_iPad/touwho_iPad/abcd.m
@@ -0,0 +1,21 @@
+//
+//  abcd.m
+//  touwho_iPad
+//
+//  Created by apple on 15/11/19.
+//  Copyright © 2015年 touhu.com. All rights reserved.
+//
+
+#import "abcd.h"
+
+@implementation abcd
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
